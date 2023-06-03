e492468892a581d471f24d1da9a543e8bfdbd42a
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2023-06-03 21:26:33 +0700

    template dev 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import Info from "./Info";
+
 
 function App() {
   return (
-    <h1>.</h1>
+    <div>
+      {/* search bar */}
+      <section class="p-5 mt-9">
+        <div class="ms-0 mb-5">
+          <h2 class="text-2xl text-center text-white roboto"><b>Search Pokemon</b></h2>
+          <div class="py-5">
+            <div class="px-9">
+              <form class="text-center" action="/search" method="post">
+                <input type="text" id="search-bar" placeholder="Search" aria-label="Search" 
+                  class="block w-full p-2 border border-gray-300 rounded-lg" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* info */}
+      <Info />
+    </div>
   );
 }
 
---
+++ src/Info.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Info() {
+  
+  return (
+    <div>
+        <section class="p-5 mt-9">
+            <div class="ms-0 mb-5">
+                <div class="container mx-auto">
+
+                    <div class="grid grid-cols-3 gap-4">
+                        {/* col 1, img */}
+                        <div class="py-5 px-2">
+                            <img src="irasutoya.png" class="" />
+                            <h1 class="text-white text-2xl text-center my-3"><b>Pokemon</b> - poxedex</h1>
+                            <div class="mx-2 my-3">
+                                <span class="bg-slate-900 text-white rounded-full px-3 mx-1 py-1 my-3">
+                                    element
+                                </span>
+                                <span class="bg-slate-900 text-white rounded-full px-3 mx-1 py-1 my-3">
+                                    element
+                                </span>
+                            </div>
+                        </div>
+
+                        {/* col 2-3 desc */}
+                        <div class="col-span-2 bg-gray-800/75 rounded-lg py-5 px-2">
+                            <div class="px-5">
+                                <h1 class="text-white">test1: namd</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
